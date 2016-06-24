3662ec36e4a0fb9d3c2b8018f31464250a57bf2b
delad20 <vlad.crishan20@gmail.com>
2016-06-25 02:01:54 +0300

    Added js + font

---
+++ js/deBlindeteScript.js
@@ -0,0 +1,13 @@
+/*jslint browser: true*/
+/*global $, jQuery, alert*/
+
+$(document).ready(function () {
+    "use strict";
+    
+    // Active navbar button
+    $(".nav a").on("click", function () {
+        $(".nav").find(".active").removeClass("active");
+        $(this).parent().addClass("active");
+    });
+    
+});
