0704023b6386141ab388918120ba50f40293c854
delad20 <vlad.crishan20@gmail.com>
2016-06-26 23:29:46 +0300

    Changing things around and adding test images
    On my way to something

--- js/deBlindeteScript.js
+++ js/deBlindeteScript.js
@@ -3,11 +3,13 @@
 
 $(document).ready(function () {
     "use strict";
-    
+
     // Active navbar button
     $(".nav a").on("click", function () {
         $(".nav").find(".active").removeClass("active");
         $(this).parent().addClass("active");
     });
-    
+
 });
+
+
