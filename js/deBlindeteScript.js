362a5b67389fef91bd75a191d395c6f0e26a2e8a
delad20 <vlad.crishan20@gmail.com>
2016-07-15 23:38:23 +0300

    Header + glyph size + map

--- js/deBlindeteScript.js
+++ js/deBlindeteScript.js
@@ -12,4 +12,20 @@ $(document).ready(function () {
 
 });
 
+function initialize() {
+    
+    var myLatLng = {lat: 46.141043, lng: 21.315124};
+    
+    var mapProp = {
+        center: myLatLng,
+        zoom: 15
+    };
 
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
