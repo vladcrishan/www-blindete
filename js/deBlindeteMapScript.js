c71d22eb6b292970dd6a5c30729793ced8c5f180
delad20 <vlad.crishan20@gmail.com>
2016-08-07 16:31:51 +0300

    Add footer with js + making separate js for google map

---
+++ js/deBlindeteMapScript.js
@@ -0,0 +1,16 @@
+function initialize() {
+    var myLatLng = {
+        lat: 46.141043,
+        lng: 21.315124
+    };
+    var mapProp = {
+        center: myLatLng,
+        zoom: 15
+    };
+    var map = new google.maps.Map(document.getElementById("map"), mapProp);
+    var marker = new google.maps.Marker({
+        position: myLatLng,
+        map: map
+    });
+}
+google.maps.event.addDomListener(window, 'load', initialize);
--- js/deBlindeteScript.js
+++ js/deBlindeteScript.js
@@ -11,24 +11,10 @@ $(document).ready(function () {
 
 //FancyBox
 $(document).ready(function () {
-    alert("doi");
     $(".fancybox").fancybox();
 });
 
-//Google Map
-function initialize() {
-    var myLatLng = {
-        lat: 46.141043,
-        lng: 21.315124
-    };
-    var mapProp = {
-        center: myLatLng,
-        zoom: 15
-    };
-    var map = new google.maps.Map(document.getElementById("map"), mapProp);
-    var marker = new google.maps.Marker({
-        position: myLatLng,
-        map: map
-    });
-}
-google.maps.event.addDomListener(window, 'load', initialize);
+//Include footer
+$(function () {
+    $("#footer").load("Footer.html");
+});
