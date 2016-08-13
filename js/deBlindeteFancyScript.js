a4fa2923ab4fe91ba1c94dc7b3c0e39097c0c74f
delad20 <vlad.crishan20@gmail.com>
2016-08-14 02:33:31 +0300

    Work on img-cont + some clean up

---
+++ js/deBlindeteFancyScript.js
@@ -0,0 +1,18 @@
+//FancyBox
+$(document).ready(function () {
+//    $(".fancybox").fancybox();
+
+    //Thumbnails
+    $(document).ready(function () {
+        $(".fancybox-thumb").fancybox({
+            prevEffect: 'none',
+            nextEffect: 'none',
+            helpers: {
+                thumbs: {
+                    width: 75,
+                    height: 50
+                }
+            }
+        });
+    });
+});
--- js/deBlindeteScript.js
+++ js/deBlindeteScript.js
@@ -9,25 +9,6 @@ $(document).ready(function () {
 
 });
 
-//FancyBox
-$(document).ready(function () {
-    $(".fancybox").fancybox();
-
-    //Thumbnails
-    $(document).ready(function () {
-        $(".fancybox-thumb").fancybox({
-            prevEffect: 'none',
-            nextEffect: 'none',
-            helpers: {
-                thumbs: {
-                    width: 75,
-                    height: 50
-                }
-            }
-        });
-    });
-});
-
 //Include footer
 $(function () {
     $("#footer").load("Footer.html");
