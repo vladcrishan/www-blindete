fc6c381799f44b3aea19a9412442b485c82b412d
Vlad Crișan <vlad.crisan@staff.bluematrix.com>
2019-09-13 14:44:08 +0300

    Footer in router fix

--- src/routes/FooterRoute.jsx
+++ src/routes/FooterRoute.jsx
@@ -1,26 +1,12 @@
-// Leave it here just in case I change my mind and want to use navbar
-
 import React from 'react'
 import { Route } from 'react-router-dom'
 import Footer from '../components/Footer'
-import styled from 'styled-components'
 
 const FooterRoute = props => (
   <React.Fragment>
-    <Page>
-      <Route {...props} />
-    </Page>
+    <Route {...props} />
     <Footer />
   </React.Fragment>
 )
-
-const Page = styled.section`
-  /* max-width: 935px; */
-  /* margin: auto; */
-  /* padding-top: 105px; */
-  /* padding-left: 16px; */
-  /* padding-right: 16px; */
-  /* background: #dcaec7; */
-`
 
 export default React.memo(FooterRoute)
